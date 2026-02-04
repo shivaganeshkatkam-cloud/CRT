af9a1f4bdc49d76871f180ac7e7e38bd08090c3e
shivaganeshkatkam-cloud <shivaganeshkatkam@gmail.com>
2026-02-04 15:48:00 +0530

    04-02-26

---
+++ IV-SEM/Python/Practice/M02_Logic _Building_ and _Patterns/PS02_Number_series.py
@@ -0,0 +1,35 @@
+
+#print even and odd numbers
+'''n=int(input())
+for i in range(0,n):
+    if i%2!=0:
+       print(i)
+        
+#print n Fibicocci
+n=int(input())
+a=0
+b=1
+for i in range(0,n):
+    print(a,end=" ")
+    c=a+b
+    a,b=b,c
+    x
+n=int(input())
+for i in range(0,n):
+    print()
+    for j in range(1,11):
+        s=i*j
+        print(i,"x",j,"=",s)
+       
+n=int(input())
+for i in range(1,n+1):
+    print(i**3,end=' ')
+     
+n=int(input())
+for i in range(n):
+    if i%2==0:
+        print(-i)
+    else:
+        print(i)
+        '''
+
---
+++ IV-SEM/Python/Practice/M02_Logic _Building_ and _Patterns/s01/PS01_Digits_problems.py
@@ -0,0 +1,32 @@
+#1. count the number of digits in a number
+'''
+n=int(input())
+count=0
+while(n>0):
+    count+=1
+    n//=10
+print(count)
+
+f=int(input())
+s=0
+while (f>0):
+    s+=f%10
+    f//=10
+print(sum)
+
+n=int(input())
+even=0
+odd=0
+while(n>0):
+    temp=n%10
+    if (temp%2==0):
+        even+=1
+    else:
+        odd+=1
+    n//=10
+print(even)
+print(odd)
+'''
+n=int(input())
+so=0
+
